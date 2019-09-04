10a318712e8f16a319472f15fa5101af17d20aa8
Amitesh Rai <amiteshrai@outlook.com>
2019-09-04 21:18:29 +0530

    completed classes

--- src/oop/classes.py
+++ src/oop/classes.py
@@ -42,10 +42,97 @@ print(p2)
 
 
 ## Wrong Use of Class Variables
-class Player:
+class PlayerWithIssue:
     team = "India"  # class var
-    formerTeam = []  # wrong use of class var
+    former_teams = []  # wrong use of class var
 
     def __init__(self, name):
         super().__init__()
         self.name = name  # instance var
+
+    def __str__(self):
+        return f"""
+            Player Info
+        Name : {self.name},
+        Team : {self.team},
+        Former Teams : {self.former_teams}
+        """
+
+
+p1 = PlayerWithIssue("Shikhar Dhawan")
+p1.former_teams.append("India U19 - 2004")
+print(p1)
+p2 = PlayerWithIssue("Yuvraj Singh")
+p2.former_teams.append("India U19 - 2001")
+print(p2)
+
+# Change class var
+p2.former_teams = None
+print(p1, p2)
+print(PlayerWithIssue.former_teams)
+
+
+class PlayerWithoutIssue:
+    team = "India"  # class var
+
+    def __init__(self, name):
+        super().__init__()
+        # instance vars
+        self.name = name
+        self.former_teams = list()
+
+    def __str__(self):
+        return f"""
+            Player Info
+        Name : {self.name},
+        Team : {self.team},
+        Former Teams : {self.former_teams}
+        """
+
+
+p1 = PlayerWithoutIssue("Shikhar Dhawan")
+p1.former_teams.append("India U19 - 2004")
+p2 = PlayerWithoutIssue("Yuvraj Singh")
+p2.former_teams.append("India U19 - 2001")
+print(p1, p2)
+
+# After change
+p1.former_teams = None
+print(p1, p2)
+
+
+class PlayerWithMembers:
+    # team = "India"
+    team_members = set()
+
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+        self.former_teams = list()
+        self.team_members.add(self.name)
+
+    def __str__(self):
+        return f"""
+            Player Info
+        Name : {self.name},
+        Team : {self.team},
+        Team Members : {self.team_members}
+        Former Teams : {self.former_teams}
+        """
+
+    @property
+    def team(self):
+        return "India"
+
+
+p1 = PlayerWithMembers("Shikhar Dhawan")
+p1.former_teams.append("India U19 - 2004")
+p2 = PlayerWithMembers("Rohit Sharma")
+p2.former_teams.append("India U19 - 2005")
+p3 = PlayerWithMembers("MS Dhoni")
+p3.former_teams.append("India Test")
+
+print(p1, p2, p3)
+# print(dir(PlayerWithMembers.team.fget()))
+print(PlayerWithMembers.team.fget(p2))
+print(type(p1.team))
